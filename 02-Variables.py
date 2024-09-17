d7edcdc3eaea52d1e8591d3dfee91957cfee9636
VolumiDev <diego.martga@gmail.com>
2024-09-17 09:43:30 +0200

    02 - Variables

---
+++ 02-Variables.py
@@ -0,0 +1,63 @@
+## Day 02: 303 Days of Python Programming
+
+first_name = "Diego"
+lastname = "Martin"
+full_name = first_name+lastname
+country = "España"
+city = "Salamanca"
+age = 35
+year = 2024
+is_married = True
+is_true = True
+is_ligth = False
+var_1, var_2, var_3 = 4, 5, 6
+
+print(type(first_name))
+print(type(lastname))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_ligth))
+print(type(var_1), type(var_2), type(var_3))
+
+print('La longitud de mi nombre es de:', len(first_name), 'y la de mi firstname es:', len(lastname))
+
+num_one, num_two = 5, 4
+addition = num_one + num_two
+print(addition)
+substraction = num_two - num_two
+print(substraction)
+multiplication = num_one * num_two
+print(multiplication)
+division = num_one / num_two
+print(division)
+modulus = num_one % num_two
+print(modulus)
+power = num_one ** num_two
+print(power)
+florr_div= num_one // num_two
+print(florr_div)
+
+print("Vamos a calcular el area, y la longitud de una circuneferencia de radio 30")
+radius = 30
+area_of_circle = 3.14 * (radius**2)
+circulum_of_cicle = 2 * 3.14 * radius
+print('El area es de:', area_of_circle, 'y la longitud es de:', circulum_of_cicle)
+
+radius = int(input("Introduce el radio que deseeas calcular"))
+area_of_circle = 3.14 * (radius**2)
+circulum_of_cicle = 2 * 3.14 * radius
+print('El area es de:', area_of_circle, 'y la longitud es de:', circulum_of_cicle)
+
+
+
+
+
+
+
+
+
